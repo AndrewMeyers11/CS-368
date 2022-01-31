6f1b9396e49139b46c1002d616bc994692a1ff4c
Andrew Meyers <ajmeyers4@wisc.edu>
2022-01-31 16:58:15 -0600

    Added C++ src file for P0

---
+++ P0/main.cpp
@@ -0,0 +1,12 @@
+// Name: 	Andrew Meyers
+// WiscID:	ajmeyers4
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, user." << endl;
+    cout << "Nice to meet you!" << endl;
+}
